7c83ce14a7991042ab03fb902e0490df33d94b2e
ferryal <Ferryal21@gmail.com>
2018-01-26 15:24:50 +0700

    add routing genres

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,73 @@ router.get('/api/action', (req, res) => {
     });
 });
 
+//get data CRIME
+router.get('/api/crime', (req, res) => {
+  const url = 'https://api.themoviedb.org/3/genre/80/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      let movies = data.results;
+      res.render(`crime`,{
+        title: `Crime Movies`,
+        data: movies
+      })
+    })
+})
+
+//get data drama
+router.get('/api/drama', (req, res) => {
+  const url = 'https://api.themoviedb.org/3/genre/18/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      let movies = data.results;
+      res.render(`drama`,{
+        title: `Drama Movies`,
+        data: movies
+      })
+    })
+})
+
+//get data history
+router.get('/api/history', (req, res) => {
+  const url = 'https://api.themoviedb.org/3/genre/36/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      let movies = data.results;
+      res.render(`history`,{
+        title: `History Movies`,
+        data: movies
+      })
+    })
+})
+
+//get data HORROR
+router.get('/api/horor', (req, res) => {
+  const url = 'https://api.themoviedb.org/3/genre/27/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      let movies = data.results;
+      res.render(`horor`,{
+        title: `Horror Movies`,
+        data: movies
+      })
+    })
+})
+
+//get data SCI_FI
+router.get('/api/science-fiction', (req, res) => {
+  const url = 'https://api.themoviedb.org/3/genre/878/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      let movies = data.results;
+      res.render(`science-fiction`,{
+        title: `Sci-Fi Movies`,
+        data: movies
+      })
+    })
+})
 module.exports = router;
